010c1de27c84c4b7f13fd1053727e5d8f7fda08d
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2025-12-03 14:15:49 +0100

    Use PointcloudSensor::loadPLY().

--- tasks/PointcloudMapper.cpp
+++ tasks/PointcloudMapper.cpp
@@ -221,34 +221,6 @@ void PointcloudMapper::sendMap()
 	mScansReceived = 0;
 }
 
-bool PointcloudMapper::loadPLYMap(const std::string& path)
-{
-	PointCloud::Ptr pcl_cloud(new PointCloud());
-	pcl::PLYReader ply_reader;
-	if(ply_reader.read(path, *pcl_cloud) >= 0)
-	{
-		Transform pc_tr(pcl_cloud->sensor_orientation_.cast<ScalarType>());
-		pc_tr.translation() = pcl_cloud->sensor_origin_.block(0,0,3,1).cast<ScalarType>();
-		PointCloudMeasurement::Ptr initial_map(new PointCloudMeasurement(pcl_cloud, _robot_name.get(), mPclSensor->getName(), pc_tr));
-		try
-		{
-			VertexObject root_node = mGraph->getVertex(0);
-			mMapper->addExternalMeasurement(initial_map, root_node.measurementUuid,
-				Transform::Identity(), Covariance<6>::Identity(), "ply-loader");
-			addScanToMap(initial_map, Transform::Identity());
-			return true;
-		}
-		catch(std::exception& e)
-		{
-			mLogger->message(ERROR, (boost::format("Adding initial point cloud failed: %1%") % e.what()).str());
-		}
-	}else
-	{
-		mLogger->message(ERROR, (boost::format("Failed to load a-priori PLY map %1%") % path).str());
-	}
-	return false;
-}
-
 bool PointcloudMapper::setLog_level(boost::int32_t value)
 {
 	switch(value)
@@ -391,9 +363,14 @@ bool PointcloudMapper::configureHook()
 	mMultiLayerMap.translate(Eigen::Vector3d(mGridConf.min_x, mGridConf.min_y, 0));
 
 	// load a-priori map file
-	if(!_apriori_ply_map.value().empty() && loadPLYMap(_apriori_ply_map.value()))
+	if(!_apriori_ply_map.value().empty())
 	{
+		mPclSensor->loadPLY(_apriori_ply_map.value(), _robot_name.value());
 		mScansAdded++;
+		const VertexObject& ply_vertex = mGraph->getVertex(mPclSensor->getLastVertexId());
+		addScanToMap(castToPointcloud(
+			mStorage->get(ply_vertex.measurementUuid)),
+			ply_vertex.correctedPose);
 	}
 	
 	return true;
